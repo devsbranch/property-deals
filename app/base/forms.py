98faa6f77ca26cce6374f518827d04f5e66b10e8
Jachin <jachin@pop-os.localdomain>
2020-12-23 09:29:34 +0200

    add new forms, db models and routes

--- app/base/forms.py
+++ app/base/forms.py
@@ -4,17 +4,28 @@ Copyright (c) 2020 - DevsBranch
 """
 
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
-from wtforms.validators import InputRequired, Email, DataRequired
+from wtforms import StringField, PasswordField, TextAreaField, IntegerField,SubmitField, FileField
+from wtforms.validators import InputRequired, Email, DataRequired,Length
+from flask_wtf.file import FileAllowed
+from wtforms.fields import MultipleFileField
+
 
 ## login and registration
-
 class LoginForm(FlaskForm):
-    username = TextField    ('Username', id='username_login'   , validators=[DataRequired()])
-    password = PasswordField('Password', id='pwd_login'        , validators=[DataRequired()])
+    username = StringField('Username', id='username_login', validators=[DataRequired()])
+    password = PasswordField('Password', id='pwd_login', validators=[DataRequired()])
+
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username'     , id='username_create' , validators=[DataRequired()])
-    email    = TextField('Email'        , id='email_create'    , validators=[DataRequired(), Email()])
-    password = PasswordField('Password' , id='pwd_create'      , validators=[DataRequired()])
+    username = StringField('Username', id='username_create', validators=[DataRequired()])
+    email = StringField('Email', id='email_create', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', id='pwd_create', validators=[DataRequired()])
 
+
+class ListPropertyForm(FlaskForm):
+    prop_name = StringField('Property Name', validators=[DataRequired()])
+    prop_desc = TextAreaField('Property Description', validators=[DataRequired(), Length(min=10, max=256)])
+    prop_photos = MultipleFileField('Upload photos of your property', validators=[FileAllowed(['jpg', 'png'])])
+    prop_price = StringField('Price (ZMW)', validators=[DataRequired(), Length(min=1, max=10)])
+    prop_location = StringField('Property location', validators=[DataRequired(), Length(min=1, max=64)])
+    submit = SubmitField('Sale Property')
--- app/home/routes.py
+++ app/home/routes.py
@@ -2,43 +2,47 @@
 """
 Copyright (c) 2020 - DevsBranch
 """
-
+import os
 from app.home import blueprint
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, current_app
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+from app.base.forms import ListPropertyForm
+from app.base.models import Property, PropertyImage
+from app import db
+from werkzeug.utils import secure_filename
+
 
 @blueprint.route('/index')
 @login_required
 def index():
-
     return render_template('index.html', segment='index')
+
 
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
-
     try:
 
-        if not template.endswith( '.html' ):
+        if not template.endswith('.html'):
             template += '.html'
 
         # Detect the current page
-        segment = get_segment( request )
+        segment = get_segment(request)
 
         # Serve the file (if exists) from app/templates/FILE.html
-        return render_template( template, segment=segment )
+        return render_template(template, segment=segment)
 
     except TemplateNotFound:
         return render_template('page-404.html'), 404
-    
+
     except:
         return render_template('page-500.html'), 500
 
-# Helper - Extract current page name from request 
-def get_segment( request ): 
 
+# Helper - Extract current page name from request
+def get_segment(request):
     try:
 
         segment = request.path.split('/')[-1]
@@ -46,7 +50,42 @@ def get_segment( request ):
         if segment == '':
             segment = 'index'
 
-        return segment    
+        return segment
 
     except:
-        return None  
+        return None
+
+
+@blueprint.route('/list-property', methods=['GET', 'POST'])
+def create_listing():
+    form = ListPropertyForm()
+    if request.method == 'POST':
+        prop_info = Property(property_name=form.prop_name.data,
+                             property_desc=form.prop_desc.data,
+                             property_price=form.prop_price.data,
+                             property_location=form.prop_location.data,
+                             user_id=current_user.id)
+
+        db.session.add(prop_info)
+        db.session.commit()
+
+        # get the id of the property added above and used it to link the image files to the property.
+        get_prop_id = db.session.query(Property).order_by(Property.date.desc())
+        photos = form.prop_photos.data
+
+        for photo in photos:
+            prop_pictures = PropertyImage(image_name=secure_filename(photo.filename),
+                                          property_id=get_prop_id[0].id)
+            db.session.add(prop_pictures)
+            db.session.commit()
+
+        # gets image files as a list uploaded by user in the form
+        img_files = request.files.getlist('prop_photos')
+        # saves image files uploaded to app/base/static/property_images/
+        for img_file in img_files:
+            filename = secure_filename(img_file.filename)
+            file_path = os.path.join(current_app.root_path, 'base/static/property_images', filename)
+            img_file.save(file_path)
+    
+        return redirect(url_for('base_blueprint.route_default'))
+    return render_template('create_listing.html', form=form)
