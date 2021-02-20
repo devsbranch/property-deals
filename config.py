01bb148d316e9e6cda89eba398929bc2b4d4aa3e
Jachin Manda <manda.jachin482@gmail.com>
2021-02-20 14:14:49 +0200

    fix flask db migration issue

--- config.py
+++ config.py
@@ -9,10 +9,9 @@ class Config(object):
 
     # Set up the App SECRET_KEY
     SECRET_KEY = config("SECRET_KEY", default="S#perS3crEt_007")
-
     # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "db.sqlite3")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = config("SQLALCHEMY_DATABASE_URI")
+    # SQLALCHEMY_TRACK_MODIFICATIONS = config("SQLALCHEMY_TRACK_MODIFICATIONS")
     JWT_SECRET_KEY = "This!isAnAPIofSomeSort"
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
