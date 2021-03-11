3f904990d36399e52fa7760890d9dab804fece11
Jachin Manda <manda.jachin482@gmail.com>
2021-03-11 12:57:49 +0200

    add functions  to process image uploads

--- app/base/utils.py
+++ app/base/utils.py
@@ -1,46 +1,17 @@
-import json
-import os
 import uuid
+import numpy as np
 from datetime import datetime
-from PIL import Image
-from flask_login import current_user
-from werkzeug.utils import secure_filename
-from app import db, s3
+from app import redis_client
 from config import S3_BUCKET_CONFIG
-from app.base.models import Property
 
 bucket = S3_BUCKET_CONFIG["S3_BUCKET"]
 s3_prop_image_dir = S3_BUCKET_CONFIG["PROP_ASSETS"]
-s3_temp_dir = S3_BUCKET_CONFIG["TEMP_DIR"]
 s3_user_image_dir = S3_BUCKET_CONFIG["USER_ASSETS"]
-
-
-def save_images_to_temp_folder(image_files):
-    """
-    Creates a temporary folder for storing image objects and returns
-    name of the temporary folder and the list of the filenames that were saved to S3 in the
-    same folder.
-    """
-    from app.tasks import image_uploader
-    dir_name = str(uuid.uuid4())[:8] + "/"
-
-    image_list = []
-    for image_file in image_files:
-        random_str = str(uuid.uuid4())[:13].replace("-", "")
-        checked_filename = secure_filename(image_file.filename)
-        _, file_ext = os.path.splitext(checked_filename)  # get file extension
-        clean_filename = f"{random_str}{file_ext}"
-        image_list.append(clean_filename)
-        image_uploader(image_file, s3_temp_dir + dir_name + clean_filename)
-
-    return dir_name, image_list
 
 
 def generate_img_dir_name():
     """
-    This function generates a random string which is concatenated together with the current date and time, then
-    returns it. This will be used by the property_image_handler() and update_property_images() function as name for
-    the directory where the image files should be saved.
+    This function generates a random string tobe used as a directory name where images will be saved on S3
     """
     suffix = str(uuid.uuid4())[:15].replace("-", "")
     current_date = datetime.utcnow()
@@ -52,60 +23,26 @@ def generate_img_dir_name():
     return dir_name
 
 
-def property_image_handler(temp_dir, image_file_list):
+def save_to_redis(image_file_list):
     """
-    Save the images. First a new directory is created where the images are saved. The process_images() is called and
-    runs in the background. Then the directory name and image filenames are returned in one list.
+    Saves the images to redis by converting it to bytes. Returns the keys of each imagein a list.
     """
-
-    from app.tasks import process_images
-
-    generated_dirname = generate_img_dir_name()
-    images_list = [generated_dirname]
-    for filename in image_file_list:
-        images_list.append(filename)
-        process_images.delay(filename, temp_dir, generated_dirname)
-    return images_list
-
-
-def update_property_images(temp_dir, image_file_list, prop_id):
-    """
-    Updates the images. First the old directory is deleted and then a new one is created
-    where the images are saved. The process_images() is called and runs in the background.
-    Then the directory name and image filenames are returned in one list.
-    """
-    from app.tasks import process_images, delete_img_objs
-
-    prop_to_update = Property.query.get(prop_id)
-    prop_images = json.loads(prop_to_update.photos)
-    path_to_del = s3_prop_image_dir + prop_to_update.image_folder
-    delete_img_objs.delay(bucket, path_to_del, prop_images)
-
-    generated_dir_name = generate_img_dir_name()
-
-    images_list = [generated_dir_name]
-    for filename in image_file_list:
-        images_list.append(filename)
-        process_images.delay(filename, temp_dir, generated_dir_name)
-    return images_list
+    redis_img_keys = []
+    for file in image_file_list:
+        rand_str = str(uuid.uuid4())[:8]  # generate a random string to set as key for the object in redis
+        redis_img_keys.append(rand_str)
+        # convert file to bytes
+        img_to_bytes = np.array(np.frombuffer(file.read(), np.uint8)).tobytes()
+        redis_client.set(rand_str, img_to_bytes)
+    return redis_img_keys
 
 
-def save_profile_picture(image_file):
-    """
-    Handle the uploaded image file. The file is renamed to a random hex, then the image object is process by PIL
-    image library then saved.
-    """
-    from app.tasks import update_profile_image
+def resize_images(key_list, upload_dir):
+    from app.tasks import image_process
 
-    dir_name = generate_img_dir_name()
-    _, file_ext = os.path.splitext(image_file.filename)
-    image_filename = f"{str(uuid.uuid4())[:8]}{file_ext}"
-
-    img = Image.open(image_file)
-    img.thumbnail((200, 250))
-    img.save(image_filename)
-    current_user.photo = s3_user_image_dir + dir_name + image_filename
-    db.session.commit()
-
-    content_type = image_file.content_type
-    update_profile_image.delay(dir_name, image_filename, content_type)
+    filenames = [upload_dir]  # will contain list of image filenames in the destination folder
+    for r_key in key_list:
+        rand_str = str(uuid.uuid4())[:8] + ".jpg"  # will be used as filename for the file when uploading to S3
+        image_process.delay(r_key, s3_prop_image_dir + upload_dir, rand_str)  # uploads the image file to S3
+        filenames.append(rand_str)
+    return filenames
--- app/tasks.py
+++ app/tasks.py
@@ -1,7 +1,8 @@
 import json
 import os
+import io
 from PIL import Image
-from app import celery, s3
+from app import celery, s3, redis_client
 from app.base.utils import update_property_images
 from config import S3_BUCKET_CONFIG
 
@@ -12,74 +13,34 @@ s3_user_image_dir = S3_BUCKET_CONFIG["USER_ASSETS"]
 
 
 @celery.task()
-def process_images(filename, tmp_dir, dir_to_save):
+def image_process(redis_img, upload_dir, filename):
     """
-    This function processes the image files, using the PIL image library. The filename argument is
-    used to create writable binary object, the object is overwritten by the data of the image downloaded
-    from s3 using boto3 client. Then the image processed using PIL is uploaded back to s3 using boto3 client.
-    The name of this object the filename argument. The image is deleted in the temp dir for cleanup.
+    Process image files, using the PIL image library, upload to S3 and delete the image from redis.
     """
-    try:
-        img_obj = open(filename, "wb")
-        s3.download_fileobj(bucket, f"{s3_temp_dir + tmp_dir}{filename}", img_obj)
-        pil_image_obj = Image.open(filename)
-        pil_image_obj.thumbnail((800, 800))
-        pil_image_obj.save(filename)
-
-        with open(filename, "rb") as data_obj:
-            image_uploader(data_obj, s3_prop_image_dir + dir_to_save + filename)
-        os.remove(filename)
-        delete_img_objs(bucket, s3_temp_dir + tmp_dir, file_obj=filename)
-    except Exception as e:
-        return e
-
-
-@celery.task()
-def update_prop_images(temp_dir, image_file_list, prop_id):
-    from app.base.models import Property
-
-    images_list = update_property_images(temp_dir, image_file_list, prop_id)
-    img_list_to_json = json.dumps(images_list)
-    Property.update_property_images(images_list[0], img_list_to_json, prop_id)
-    return "Images Updated"
-
-
-@celery.task()
-def update_profile_image(dir_name, filename, content_type):
-    s3.upload_fileobj(
-        open(filename, "rb"),
-        bucket,
-        f"{s3_user_image_dir}{dir_name}{filename}",
-        ExtraArgs={
-            "ACL": "public-read",
-            "ContentType": content_type
-        }
-    )
-    os.remove(filename)
-
-
-@celery.task()
-def image_uploader(image_file, path_to_save):
+    bytes_img = redis_client.get(redis_img)
+    img = Image.open(io.BytesIO(bytes_img))
+    img.thumbnail((800, 800))
+    img.save(redis_img + ".jpg", format="JPEG")
     try:
         s3.upload_fileobj(
-            image_file,
+            open(redis_img + ".jpg", "rb"),  # This is the name of the file saved on line 23
             bucket,
-            path_to_save,
+            upload_dir + filename,
             ExtraArgs={
                 "ACL": "public-read",
                 "ContentType": "image/jpeg"
             }
         )
-    except Exception as e:
-        return e
+        os.remove(redis_img)  # Delete the file that was saved temporary on local
+        redis_client.delete(redis_img)
+    except Exception as err:
+        return err
 
 
 @celery.task()
-def delete_img_objs(bucket, dir_to_del, image_list=None, file_obj=None):
+def delete_img_obj(bucket, dir_to_del, image_list=None, filename=None):
     """
-    Deletes objects on S3 either by iterating through the image_list and concatenating the
-    dir_to_del and the filename in image_list to make a complete path to the object to
-    be deleted or using the file_obj argument.
+    Delete image objects stored on S3, either one object or multiple at the same time.
     """
     if image_list:
         for filename in image_list:
@@ -90,6 +51,6 @@ def delete_img_objs(bucket, dir_to_del, image_list=None, file_obj=None):
     else:
         s3.delete_object(
             Bucket=bucket,
-            Key=dir_to_del + file_obj
+            Key=dir_to_del + filename
         )
     return "deletion task completed"
