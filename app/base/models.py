a4d0e5770580a24a00ed652a82c8d08a15d28ced
Jachin Manda <manda.jachin482@gmail.com>
2021-04-14 22:10:11 +0200

    add module for search

--- app/base/models.py
+++ app/base/models.py
@@ -1,60 +1,9 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
-from flask_login import UserMixin
-from sqlalchemy import Binary, Column, Integer, String
-
-from app import db, login_manager
-
-from app.base.util import hash_pass
-
-
-# class User(db.Model, UserMixin):
-#     __tablename__ = 'User'
-#
-#     id = Column(Integer, primary_key=True)
-#     username = Column(String, unique=True)
-#     email = Column(String, unique=True)
-#     password = Column(Binary)
-#
-#     def __init__(self, **kwargs):
-#         for property, value in kwargs.items():
-#             # depending on whether value is an iterable or not, we must
-#             # unpack it's value (when **kwargs is request.form, some values
-#             # will be a 1-element list)
-#             if hasattr(value, '__iter__') and not isinstance(value, str):
-#                 # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
-#                 value = value[0]
-#
-#             if property == 'password':
-#                 value = hash_pass(value)  # we need bytes here (not plain str)
-#
-#             setattr(self, property, value)
-#
-#     def __repr__(self):
-#         return str(self.username)
-
-
-# @login_manager.user_loader
-# def user_loader(id):
-#     return User.query.filter_by(id=id).first()
-#
-#
-# @login_manager.request_loader
-# def request_loader(request):
-#     username = request.form.get('username')
-#     user = User.query.filter_by(username=username).first()
-#     return user if user else None
-
-
 import json
 from datetime import datetime
 from flask_login import UserMixin
 from app import db, login_manager
 from config import S3_BUCKET_CONFIG
-# from app.search import add_to_index, delete_from_index, search_docs
+from app.search import add_to_index, delete_from_index, search_docs
 
 
 class User(db.Model, UserMixin):
@@ -83,14 +32,6 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(60), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     user_properties = db.relationship("Property", backref="prop_owner", lazy=True)
-
-    @staticmethod
-    def get_all_users():
-        return [user.to_json() for user in User.query.all()]
-
-    @classmethod
-    def get_user(cls, _user_id):
-        return cls.query.get(_user_id)
 
     @classmethod
     def username_exists(cls, _username):
