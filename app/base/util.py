d2ebbf0fd961d11c4c2d9872cf99c6d18d48bcff
Jachin Manda <manda.jachin482@gmail.com>
2021-04-14 22:06:06 +0200

    update db models

--- app/base/util.py
+++ app/base/util.py
@@ -2,15 +2,16 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
- 
+
 import hashlib, binascii, os
 
 # Inspiration -> https://www.vitoshacademy.com/hashing-passwords-in-python/
+# u = User(username="jachin", email="jmda@pdeals.com", password=
 
 def hash_pass( password ):
     """Hash a password for storing."""
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
-    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),
                                 salt, 100000)
     pwdhash = binascii.hexlify(pwdhash)
     return (salt + pwdhash) # return bytes
@@ -20,9 +21,9 @@ def verify_pass(provided_password, stored_password):
     stored_password = stored_password.decode('ascii')
     salt = stored_password[:64]
     stored_password = stored_password[64:]
-    pwdhash = hashlib.pbkdf2_hmac('sha512', 
-                                  provided_password.encode('utf-8'), 
-                                  salt.encode('ascii'), 
+    pwdhash = hashlib.pbkdf2_hmac('sha512',
+                                  provided_password.encode('utf-8'),
+                                  salt.encode('ascii'),
                                   100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
