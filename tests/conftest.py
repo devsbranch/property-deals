e9484a873904682273d3a4225b94bd5432e8863b
Jachin Manda <manda.jachin482@gmail.com>
2021-06-20 13:11:26 +0200

    remove unsed imports and update configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,7 @@
-import os
 import pytest
-from flask import g
 from decouple import config
-from config import config_dict, IMAGE_UPLOAD_CONFIG
+from config import config_dict, Config
 from app import create_app, db
-from app.base.forms import SearchForm
 
 
 get_config_mode = "Debug" if config("DEBUG", default=False, cast=bool) else "Production"
@@ -69,9 +66,7 @@ def flask_app():
     app.config["TESTING"] = True
 
     # use a separate database for testing
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-        "TESTING_DB", config("TESTING_DB")
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
 
     # Disable CSRF tokens in the Forms (only valid for testing purposes!)
     app.config["WTF_CSRF_ENABLED"] = False
