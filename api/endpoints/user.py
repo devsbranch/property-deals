a6ee2635f99cf1beadabade1ced89f2786833fca
Jachin Manda <manda.jachin482@gmail.com>
2021-07-06 12:42:34 +0200

    add endpoint for adding user

--- api/endpoints/user.py
+++ api/endpoints/user.py
@@ -19,3 +19,23 @@ def get_user_by_id(user_id):
     if not user:
         return jsonify("User not found"), 404
     return jsonify(user), 200
+
+
+def add_user():
+    request_data = request.get_json()
+    try:
+        jsonschema.validate(request_data, add_user_schema)
+        if validate_username(request_data["username"]):
+            return jsonify({"message": "The username is already taken. Please try a different one."})
+        if validate_email(request_data["email"]):
+            return jsonify({"message": "The email you entered is already registered. Please try a different one."})
+
+        user = User(**request_data)
+        db.session.add(user)
+        db.session.commit()
+
+        response = Response(json.dumps(request_data), 201, mimetype="application/json")
+        return response
+    except jsonschema.exceptions.ValidationError as err:
+        print(err)
+        return jsonify({"message": "invalid data"}), 400
